cb4349624d7372823422a7617c77cd88b91b69d3
ethutami uta <ethutami@gmail.com>
2025-05-29 03:38:39 +0800

    delete several file

--- dist/routers/portfolio.router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const portfolio_controller_1 = require("../controllers/portfolio.controller");
-const router = (0, express_1.Router)();
-router.get("/", portfolio_controller_1.GetPortfoliosController);
-router.get("/search", portfolio_controller_1.SearchPortfoliosController);
-exports.default = router;
