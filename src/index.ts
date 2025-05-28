9d18729fd0b4bb9992455e6512bc45ec0bd1d6b6
ethutami uta <ethutami@gmail.com>
2025-05-29 04:17:58 +0800

    start

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+
+const port = 8010;
+const app: Application = express();
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json('Welcome to Web portofolio api')
+})
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(400).json({
+        success: false,
+        message: err.message,
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
