55bbce520ceb61cf4fdc8e61b9b6a20353796227
ethutami uta <ethutami@gmail.com>
2025-07-25 19:45:01 +0800

    restore

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app: Application = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'https://ethika-utami.vercel.app/'
+    origin: 'https://ethika-utami.vercel.app'
 }));
 
 app.use(cors());
